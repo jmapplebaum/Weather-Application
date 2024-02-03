9f97f41b48e8951b074b6a2433c332a8c48782b4
Jennah Applebaum <158118059+jmapplebaum@users.noreply.github.com>
2024-02-02 20:55:54 -0600

    Added search functionality using API

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-function searchCity(event) {
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#current-temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city-element");
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+
+}
+
+function searchCity(city) {
+let apiKey = "5b4802f40a5b2aoe7a3t7b824a662fdf";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`
+axios.get(apiUrl).then(refreshWeather);
+}
+
+
+function searchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-input");
-    let cityElement = document.querySelector("#city-element");
-    cityElement.innerHTML = searchInput.value;
+    
+    searchCity(searchInput.value);
 }
 
 let searchElement = document.querySelector("#search-form");
-searchElement.addEventListener("submit", searchCity);
+searchElement.addEventListener("submit", searchSubmit);
+
+searchCity("Chicago");
