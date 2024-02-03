6942b8e128b440642de836b6f73b7981d62b32bd
Jennah Applebaum <158118059+jmapplebaum@users.noreply.github.com>
2024-02-03 17:48:20 -0600

    Added weather condition data to js using API

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#current-temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city-element");
+    let conditionElement = document.querySelector("#condition");
+let humidityElement = document.querySelector("#humidity");
+let windSpeedElement = document.querySelector("#wind-speed");
+    
     cityElement.innerHTML = response.data.city;
+    conditionElement.innerHTML = response.data.condition.description;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)} mph`;
     temperatureElement.innerHTML = Math.round(temperature);
 
 }
