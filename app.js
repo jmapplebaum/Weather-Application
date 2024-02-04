7b0e54868fe9c0d6fc224ddcfd860ec0f1f7deea
Jennah Applebaum <158118059+jmapplebaum@users.noreply.github.com>
2024-02-03 18:06:26 -0600

    Added timeElement and formatDate function to js

--- app.js
+++ app.js
@@ -5,13 +5,27 @@ function refreshWeather(response) {
     let conditionElement = document.querySelector("#condition");
 let humidityElement = document.querySelector("#humidity");
 let windSpeedElement = document.querySelector("#wind-speed");
+let timeElement = document.querySelector("#current-time");
+let date = new Date(response.data.time * 1000)
     
     cityElement.innerHTML = response.data.city;
+    timeElement.innerHTML = formatDate(date);
     conditionElement.innerHTML = response.data.condition.description;
     humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
     windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)} mph`;
     temperatureElement.innerHTML = Math.round(temperature);
 
+}
+function formatDate(date) {
+let minutes = date.getMinutes();
+let hours = date.getHours();
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let day = days[date.getDay()];
+
+if (minutes <10) {
+    minutes = `0${minutes}`;
+}
+return `${day} ${hours}:${minutes},`;
 }
 
 function searchCity(city) {
