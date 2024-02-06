e249963614724844a96e592e852efc51e338425e
Jennah Applebaum <158118059+jmapplebaum@users.noreply.github.com>
2024-02-05 20:57:03 -0600

    Moved forecast html to a js loop

--- app.js
+++ app.js
@@ -1,51 +1,84 @@
 function refreshWeather(response) {
-    let temperatureElement = document.querySelector("#current-temperature");
-    let temperature = response.data.temperature.current;
-    let cityElement = document.querySelector("#city-element");
-    let conditionElement = document.querySelector("#condition");
-let humidityElement = document.querySelector("#humidity");
-let windSpeedElement = document.querySelector("#wind-speed");
-let timeElement = document.querySelector("#current-time");
-let date = new Date(response.data.time * 1000)
-let emojiElement = document.querySelector("#emoji");
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city-element");
+  let conditionElement = document.querySelector("#condition");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#current-time");
+  let date = new Date(response.data.time * 1000);
+  let emojiElement = document.querySelector("#emoji");
 
-emojiElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="emoji" />`;
-    
-    cityElement.innerHTML = response.data.city;
-    timeElement.innerHTML = formatDate(date);
-    conditionElement.innerHTML = response.data.condition.description;
-    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-    windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)} mph`;
-    temperatureElement.innerHTML = Math.round(temperature);
+  emojiElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="emoji" />`;
 
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  conditionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${Math.round(response.data.wind.speed)} mph`;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 function formatDate(date) {
-let minutes = date.getMinutes();
-let hours = date.getHours();
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let day = days[date.getDay()];
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
 
-if (minutes <10) {
+  if (minutes < 10) {
     minutes = `0${minutes}`;
-}
-return `${day} ${hours}:${minutes},`;
+  }
+  return `${day} ${hours}:${minutes},`;
 }
 
 function searchCity(city) {
-let apiKey = "5b4802f40a5b2aoe7a3t7b824a662fdf";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`
-axios.get(apiUrl).then(refreshWeather);
+  let apiKey = "5b4802f40a5b2aoe7a3t7b824a662fdf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(refreshWeather);
 }
 
-
 function searchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-input");
-    
-    searchCity(searchInput.value);
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+
+  searchCity(searchInput.value);
+}
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri"];
+  let forecastHtml = " ";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="forecast-day">
+            <div class="forecast-date">${day}</div>
+            <img
+              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/scattered-clouds-day.png"
+              class="forecast-emoji"
+            />
+            <div class="forecast-temperatures">
+              <div class="forecast-temperature"></div>
+              <div class="forecast-temperature">
+                59°
+                <span class="forecast-low">38º</span>
+              </div>
+            </div>
+          </div>
+        </div>`;
+  });
+  forecastElement.innerHTML = forecastHtml;
 }
 
 let searchElement = document.querySelector("#search-form");
 searchElement.addEventListener("submit", searchSubmit);
 
 searchCity("Chicago");
+displayForecast();
